d2184b5ca199f04190e5a4e40d9887c88b5eef20
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-03-18 12:19:26 +0900

    Create 不一致5語抽出.js

---
+++ 不一致5語抽出.js
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script language="javascript" type="text/javascript">
+url = `https://topi.cf/hack_wordle/words.json`;
+
+$.getJSON(url, (data) => {
+  console.log(`${data}`);
+});
+</script>
